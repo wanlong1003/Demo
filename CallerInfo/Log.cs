ab772c0822c692132979e7e2489c2c4cb66d00ca
Wanlong Liu <wanlong1003@163.com>
2018-04-03 23:32:23 +0800

    添加获取调用者信息的demo

---
+++ CallerInfo/Log.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallerInfo
+{
+    class Log
+    {
+        public static void Debug(string message, [CallerLineNumber] int line = -1, [CallerFilePath] string path=null, [CallerMemberName] string name=null)
+        {
+                Console.WriteLine(message + (line >=0? "line="+line: "") +( path==null?"":"path="+path)+(name==null?"":"name="+name));
+        }
+    }
+}
---
+++ CallerInfo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallerInfo
+{
+    /// <summary>
+    /// 获取调用者信息
+    ///   可以使用这种方法来输出错误发生的位置
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Debug("Main中调用");
+            Write();
+            Console.ReadLine();
+        }
+
+        static void Write()
+        {
+            Log.Debug("Write中调用");
+        }
+    }
+}
