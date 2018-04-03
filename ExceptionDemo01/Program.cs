31b4985a31efa72b5874959379ca80bc63d952ed
Wanlong Liu <wanlong1003@163.com>
2018-04-04 00:21:29 +0800

    异常Demo

---
+++ ExceptionDemo01/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                NoTryCatch();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.Source);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            try
+            {
+                TryCatch();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            Console.ReadLine();
+        }
+
+        static void NoTryCatch()
+        {
+            var i = 0;
+            var j = 1;
+            var r = j / i;
+        }
+
+        static void TryCatch()
+        {
+            try
+            {
+                var i = 0;
+                var j = 1;
+                var r = j / i;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
+        }
+    }
+}
